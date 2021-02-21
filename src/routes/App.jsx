b637e407ee6ee8384bca06155051826aabeca1ae
Brahianf <Brahianf@unicauca.edu.co>
2021-02-21 18:58:16 -0500

    [add] layout.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from '../components/Layout';
 
 const App = () => (
   <BrowserRouter>
-      <Switch>
-      </Switch>
+      <Layout>
+        <Switch>
+        </Switch>
+      </Layout>
   </BrowserRouter>
 );
 
