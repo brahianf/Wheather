ce1302e0892e1cff883d6cc5b0151b3e2c7b0135
Brahianf <Brahianf@unicauca.edu.co>
2021-02-21 19:25:37 -0500

    [add] conection App with store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './redux/reducers/rootReducer';
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('app'),
 );
