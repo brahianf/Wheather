ca155cc138e7fcd34a1e2afd81f001a5ac854ae9
Brahianf <Brahianf@unicauca.edu.co>
2021-03-05 22:44:30 -0500

    [Mod] connect data actions, and background image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import '../assets/styles/components/Banner.scss';
-import bannerImage from '../assets/img/banner-sky3.jpg';
+import clouds from '../assets/img/clouds.jpg';
+import clear from '../assets/img/clear.jpg'
+import snow from '../assets/img/snow.jpg';
+import rain from '../assets/img/rain.jpg';
+import drizzle from '../assets/img/drizzle.jpg'
+import thunderstorm from '../assets/img/thunderstorm.jpg'
 import map from '../assets/img/Map_pin_icon.svg'
 
 const Banner = (props) => {
-  const { city, country, description} = props;
-  if(city==undefined || country==undefined || description==undefined){
-    return(
-      <div>
-        <h3>... loading </h3>
+
+  const { data } = props;
+
+  const backgroundImage = [
+    { main: 'Clouds', image : clouds},
+    { main: 'Clear', image : clear},
+    { main: 'Snow', image : snow},
+    { main: 'Rain', image : rain},
+    { main: 'Drizzle', image : drizzle},
+    { main: 'Thunderstorm', image : thunderstorm},
+  ]
+
+  const main= data.weather[0].main;
+  const result = backgroundImage.find( back => back.main === main );
+
+  return (
+    <div className='banner fadeInDown' style={{backgroundImage: `url(${result.image})`}}>
+      <div className='banner__detail'>
+        <img src={map} alt='map'/>
+        <p>{data.name}, &nbsp; {data.sys.country} <br/>{data.weather[0].description}</p>
       </div>
-    );
-  }else {
-    return (
-      <div className='banner fadeInDown' style={{backgroundImage: `url(${bannerImage})`}}>
-        <div className='banner__detail'>
-          <img src={map} alt='map'/>
-          <p>{city}, &nbsp; {country} <br/>{description}</p>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
-export default Banner;
+const mapStateToProps = ({ dataReducer }) => dataReducer;
+
+export default connect(mapStateToProps)(Banner);
