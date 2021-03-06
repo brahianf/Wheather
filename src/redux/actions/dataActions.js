61a51b1d20133d91fc416460741751119c784c76
Brahianf <Brahianf@unicauca.edu.co>
2021-03-05 19:40:59 -0500

    [Mod] change async await dataActions

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -6,14 +6,11 @@ const API_KEY = config.weatherAppId;
 
 export const getWheater = (nameCity) => async (dispatch) => {
   try {
-    fetchData(`${URL_API}weather?q=${nameCity}&units=metric&appid=${API_KEY}`)
-      .then( data => {
-        dispatch({
-          type: 'DATA',
-          payload: data,
-        });
-      })
-      .catch(err => console.error(`Error ${err}`))
+    const data = await fetchData(`${URL_API}weather?q=${nameCity}&units=metric&appid=${API_KEY}`)
+    dispatch({
+      type: 'DATA',
+      payload: data,
+    });
   } catch (error) {
     console.error('getWheater -> err', error);
   }
@@ -21,29 +18,23 @@ export const getWheater = (nameCity) => async (dispatch) => {
 
 export const getWheaterForecast = (nameCity) => async (dispatch) => {
   try {
-    fetchData(`${URL_API}forecast?q=${nameCity}&units=metric&cnt=24&appid=${API_KEY}`)
-      .then( data => {
-        dispatch({
-          type: 'DATA_FORECAST',
-          payload: data,
-        });
-      })
-      .catch(err => console.error(`Error ${err}`))
+    const data = await fetchData(`${URL_API}forecast?q=${nameCity}&units=metric&cnt=24&appid=${API_KEY}`)
+    dispatch({
+      type: 'DATA_FORECAST',
+      payload: data,
+    });
   } catch (error) {
-    console.error('getWheater -> err', error);
+    console.error('getWheaterForecast -> err', error);
   }
 };
 
 export const getWheaterLocation = (nameCity) => async (dispatch) => {
   try {
-    fetchData(`${URL_API}weather?q=${nameCity}&units=metric&appid=${API_KEY}`)
-      .then( data => {
-        dispatch({
-          type: 'DATA_LOCATION',
-          payload: data,
-        });
-      })
-      .catch(err => console.error(`Error ${err}`))
+    const data = await fetchData(`${URL_API}weather?q=${nameCity}&units=metric&appid=${API_KEY}`);
+    dispatch({
+      type: 'DATA_LOCATION',
+      payload: data,
+    });
   } catch (error) {
     console.error('getWheaterLocation -> err', error);
   }
