a681a51eb9250e089212629940c91baf786f6353
Brahianf <Brahianf@unicauca.edu.co>
2021-03-05 23:10:23 -0500

    [mod] display items

--- src/components/AddLocation.jsx
+++ src/components/AddLocation.jsx
@@ -5,12 +5,12 @@ import map from '../assets/img/Map_pin_icon.svg'
 const AddLocation = (props) => {
   return (
     <div className="AddLocation">
-      <div className='AddLocation__button'>
-        <button>Add Locations</button>
-      </div>
       <div className='AddLocation__image'>
         <img src={map} alt='map'/>
         {/* <img></img> */}
+      </div>
+      <div className='AddLocation__button'>
+        <button>Add Locations</button>
       </div>
     </div>
   );
