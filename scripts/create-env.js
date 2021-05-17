af19e0de138037ad2ecf6e90bda695bc634eb2bc
brahianf <brahianf@unicauca.edu.co>
2021-05-16 20:18:09 -0500

    [mod] script writeFileSync

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
 
-fs.writeFileSync('./.env', `URL_API=${process.env.URL_API}\n`, `API_KEY=${process.env.API_KEY}\n`);
+fs.writeFileSync('./.env', `URL_API=${process.env.URL_API}\nAPI_KEY=${process.env.API_KEY}\n`);
